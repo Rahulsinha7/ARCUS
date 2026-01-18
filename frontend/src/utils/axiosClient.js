e6866ba821fb814aba45638fed4624490430444e
Rahulsinha7 <rahulrahulsinha777@gmail.com>
2026-01-18 15:53:57 +0530

    change axios link

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient=axios.create({
-    baseURL: 'http://localhost:3000',  
+    baseURL: 'https://arcus-1.onrender.com',  
     withCredentials: true, // Include cookies in requests
     headers:{
         'Content-Type': 'application/json',  //data is in json format
